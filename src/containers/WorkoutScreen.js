becb097a36dbdbeed76f8bf62193bc1a8a6f6eab
Justin Torrance <justin.torrance91@gmail.com>
2019-05-23 10:05:40 -0600

    Add fields to workout screen and begin styling

--- src/containers/WorkoutScreen.js
+++ src/containers/WorkoutScreen.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button, ScrollView } from 'react-native';
 
 export default class WorkoutScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text> WorkoutScreen </Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>Intense Upper Body Challenge</Text>
+          <Text>Length: 45 minutes</Text>
+          <Button  
+            title='START'
+          />
+        </View>
+        <View style={styles.exercise}>
+          <Text style={styles.textaz}>30 Seconds: </Text> 
+          <Text style={styles.textaz}>Push Ups</Text>
+        </View>
+        <View  style={styles.exercise}>
+          <Text style={styles.textaz}>30 Seconds: </Text> 
+          <Text style={styles.textaz}>Pull Ups</Text>
+        </View>
+        <View style={styles.exercise}>
+          <Text style={styles.textaz}>60 Seconds: </Text> 
+          <Text style={styles.textaz}>Rest</Text>
+        </View>
+        <View  style={styles.exercise}>
+          <Text style={styles.textaz}>30 Seconds: </Text> 
+          <Text style={styles.textaz}>Push Ups</Text>
+        </View>
+        <View  style={styles.exercise}>
+          <Text style={styles.textaz}>30 Seconds: </Text> 
+          <Text style={styles.textaz}>Push Ups</Text>
+        </View>
+        <View  style={styles.exercise}>
+          <Text style={styles.textaz}>60 Seconds: </Text> 
+          <Text style={styles.textaz}>Rest</Text>
+        </View>
+        <View  style={styles.exercise}>
+          <Text style={styles.textaz}>90 Seconds: </Text> 
+          <Text style={styles.textaz}>Cool Down</Text>
+        </View>
+      </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: flex,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  exercise: {
+    display: flex, 
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  textaz: {
+    textAlign: 'center'
   }
+
 });
