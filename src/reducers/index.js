8c1fe59b00237766c232614141ee76b5d0b322fb
Justin Torrance <justin.torrance91@gmail.com>
2019-05-28 10:19:43 -0600

    Add selected workout to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { error } from './error';
 import { loading } from './loading';
+import { selectedWorkout } from './selectedWorkout'
 
 export const rootReducer = combineReducers({
   error: error,
-  loading: loading
+  loading: loading,
+  selectedWorkout: selectedWorkout
 });
