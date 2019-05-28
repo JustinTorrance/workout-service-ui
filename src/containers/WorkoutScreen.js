2a37efc84ea69d5459ea98c415a2a7d71017c282
Justin Torrance <justin.torrance91@gmail.com>
2019-05-28 10:12:05 -0600

    Create action for storing a selected workout

--- src/containers/WorkoutScreen.js
+++ src/containers/WorkoutScreen.js
@@ -1,63 +1,72 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Button, ScrollView } from 'react-native';
+import { connect } from 'react-redux';
 
 export default class WorkoutScreen extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  fetchData = async () => {
+    const response = await fetch('https://workoutservice.herokuapp.com/api/v1/workouts')
+    const data = await response.json()
+    this.setState({ data })
+    console.log(data)
+  }
+
   render() {
+    const dataDisplay = this.state.data.map(workout => {
+      return workout
+    })
+
+    // const { name } = this.props.selectedWorkout
+
     return (
       <ScrollView>
         <View style={styles.container}>
-          <Text>Intense Upper Body Challenge</Text>
+          {/* <Text>{name}</Text> */}
           <Text>Length: 45 minutes</Text>
-          <Button  
-            title='START'
-          />
+          <Button onPress={this.fetchData} title='HOWDY'>
+
+          </Button>
+
         </View>
         <View style={styles.exercise}>
-          <Text style={styles.textaz}>30 Seconds: </Text> 
-          <Text style={styles.textaz}>Push Ups</Text>
-        </View>
-        <View  style={styles.exercise}>
-          <Text style={styles.textaz}>30 Seconds: </Text> 
-          <Text style={styles.textaz}>Pull Ups</Text>
-        </View>
-        <View style={styles.exercise}>
-          <Text style={styles.textaz}>60 Seconds: </Text> 
-          <Text style={styles.textaz}>Rest</Text>
-        </View>
-        <View  style={styles.exercise}>
-          <Text style={styles.textaz}>30 Seconds: </Text> 
-          <Text style={styles.textaz}>Push Ups</Text>
-        </View>
-        <View  style={styles.exercise}>
-          <Text style={styles.textaz}>30 Seconds: </Text> 
-          <Text style={styles.textaz}>Push Ups</Text>
-        </View>
-        <View  style={styles.exercise}>
-          <Text style={styles.textaz}>60 Seconds: </Text> 
-          <Text style={styles.textaz}>Rest</Text>
-        </View>
-        <View  style={styles.exercise}>
-          <Text style={styles.textaz}>90 Seconds: </Text> 
-          <Text style={styles.textaz}>Cool Down</Text>
+          <Text style={styles.textStyle}>{}</Text> 
+          <Text style={styles.textStyle}>{dataDisplay}</Text>
         </View>
       </ScrollView>
     )
   }
 }
 
+// export const mapStateToProps = (state) => ({
+//   // selectedWorkout: state.selectedWorkout
+// })
+
+// export default connect(mapStateToProps)(WorkoutScreen)
+
 const styles = StyleSheet.create({
   container: {
-    display: flex,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
   exercise: {
-    display: flex, 
-    flexDirection: 'row',
-    alignItems: 'center'
+    // flex: 4, 
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // textAlign: 'center'
   },
-  textaz: {
-    textAlign: 'center'
+  textStyle: {
+    textAlign: 'center',
+    flex: 2,
+    flexDirection: 'row',
+
+    // left: '50%'
   }
 
 });
